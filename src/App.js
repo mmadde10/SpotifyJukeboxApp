60e4e755755ef687c3b6bf9214760a5affd8fa5a
mmadde10 <mmadden1495@gmail.co>
2019-12-07 11:59:24 -0600

    init redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import AddTodo from "./components/AddTodo";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import TemporaryDrawer from "./components/Drawer";
+import Landing from "./components/Landing";
+import {connect} from 'react-redux';
+import {getProfileFetch} from './redux/actions';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+
+  useEffect(() => {
+    getProfileFetch();
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <TemporaryDrawer></TemporaryDrawer>
+        <AppBar>
+          <Toolbar>
+              <IconButton edge="start" className={classes.menuButton}  color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+
+            <Switch>
+              <Route path="/todo">
+                <AddTodo />
+              </Route>
+              <Route path="/signup">
+                <Signup />
+              </Route>
+              <Route path="/signin">
+                <Signin />
+              </Route>
+              <PrivateRoute  path="/landing">
+                <Landing />
+              </PrivateRoute>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  getProfileFetch: () => dispatch(getProfileFetch())
+})
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/actions.js
@@ -0,0 +1,88 @@
+// redux/actions.js
+import { ADD_TODO } from './actionTypes'
+
+let nextTodoId = 0
+export const addTodo = content => ({
+  type: ADD_TODO,
+  payload: {
+    id: ++nextTodoId,
+    content
+  }
+})
+
+export const userPostFetch = user => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/users", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({user})
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.message) {
+            console.log("hello");
+        } else {
+          localStorage.setItem("token", data.jwt)
+          dispatch(loginUser(data.user))
+        }
+      })
+  }
+}
+
+const loginUser = userObj => ({
+    type: 'LOGIN_USER',
+    payload: userObj
+})
+
+export const userLoginFetch = user => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({user})
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.message) {
+          // Here you should have logic to handle invalid login credentials.
+          // This assumes your Rails API will return a JSON object with a key of
+          // 'message' if there is an error
+        } else {
+          localStorage.setItem("token", data.jwt)
+          dispatch(loginUser(data.user))
+        }
+      })
+  }
+}
+
+export const getProfileFetch = () => {
+  return dispatch => {
+    const token = localStorage.token;
+    if (token) {
+      return fetch("http://localhost:3000/api/v1/profile", {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+        .then(resp => resp.json())
+        .then(data => {
+          if (data.message) {
+            // An error will occur if the token is invalid.
+            // If this happens, you may want to remove the invalid token.
+            localStorage.removeItem("token")
+          } else {
+            dispatch(loginUser(data.user))
+          }
+        })
+    }
+  }
+}
